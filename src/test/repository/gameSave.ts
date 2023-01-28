d3017b55bce35ccbc762f88ce5e365a402e150d8
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-28 15:20:43 -0300

    📦  FEAT: removeByGameName new method implemented

--- src/test/repository/gameSave.ts
+++ src/test/repository/gameSave.ts
@@ -6,7 +6,7 @@ import { GameSaveInterface } from "../../data/interfaces/gameSave";
 import { gameSaveRepositoryImp } from "../../data/interfaces/repository/gameSave";
 
 export class GameSaveRepositoryFake implements gameSaveRepositoryImp{
-
+    
     public gameSaves = [
         {
             createdAt: new Date("2021-05-01"),
@@ -17,6 +17,12 @@ export class GameSaveRepositoryFake implements gameSaveRepositoryImp{
             provider:"drive"
         }
     ] as GameSaveInterface[]
+    
+    async removeByGameName (gameName: string) :Promise<Either<ErrorBase, void>>{
+        const gameSave = this.gameSaves.find(item=>item.nameGame === gameName)
+        if(!gameSave) return Left.create(new GameSaveNotFoundError())
+        return Right.create(undefined)
+    }
 
     async create (gameSave: Replacer<GameSaveInterface, { id?: string , createdAt?: Date , updatedAt?: Date }>) : Promise<Either<ErrorBase,GameSaveInterface>>{
         
