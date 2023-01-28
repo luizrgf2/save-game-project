653651566986a3c2551d97d6279d5ced6c819cb8
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-28 14:18:13 -0300

    📦  FEAT: GameSaveRepositoryFake implemented

---
+++ src/test/repository/gameSave.ts
@@ -0,0 +1,29 @@
+import { Either, Left, Right } from "../../data/errors/either";
+import { ErrorBase } from "../../data/errors/errorBase";
+import { GameSaveNotFoundError } from "../../data/errors/gameSaveNotFound";
+import { Replacer } from "../../data/helpers/replace";
+import { GameSaveInterface } from "../../data/interfaces/gameSave";
+import { gameSaveRepositoryImp } from "../../data/interfaces/repository/gameSave";
+
+export class GameSaveRepositoryFake implements gameSaveRepositoryImp{
+
+    public gameSaves = [
+        {
+            createdAt: new Date("2021-05-01"),
+            updatedAt:  new Date("2021-05-01"),
+            directorySaveGame:"./src/resident-evil",
+            id:"valid1",
+            nameGame:"Resident Evil",
+            provider:"drive"
+        }
+    ] as GameSaveInterface[]
+
+    async create (gameSave: Replacer<GameSaveInterface, { id?: string , createdAt?: Date , updatedAt?: Date }>) : Promise<Either<ErrorBase,GameSaveInterface>>{
+        
+        const validGameSave = this.gameSaves.find(item=>item.id===gameSave.id)
+        
+        if(!validGameSave) return Left.create(new GameSaveNotFoundError())
+        return Right.create(validGameSave)
+    }
+    
+}
