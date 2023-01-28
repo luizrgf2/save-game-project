02e768221140cb909053a8a17f2e0e6a9619662e
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-28 17:10:13 -0300

    📦  FEAT: new method getAll add

--- src/data/interfaces/repository/gameSave.ts
+++ src/data/interfaces/repository/gameSave.ts
@@ -11,4 +11,5 @@ import { GameSaveInterface } from "../gameSave";
 export interface gameSaveRepositoryImp{
     create:(gameSave:Replacer<GameSaveInterface,{id?:string,createdAt?:Date,updatedAt?:Date}>)=>Promise<Either<ErrorBase,GameSaveInterface>>
     removeByGameName:(gameName:string)=>Promise<Either<ErrorBase,void>>
+    getAll:() => Promise<Either<ErrorBase,GameSaveInterface[]>>
 }
