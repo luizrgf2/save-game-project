284fd1e15de0e8a0dedbaeea1385ff209fcdf753
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-28 17:12:05 -0300

    📦  FEAT: getAll method implemented

--- src/test/repository/gameSave.ts
+++ src/test/repository/gameSave.ts
@@ -15,9 +15,38 @@ export class GameSaveRepositoryFake implements gameSaveRepositoryImp{
             id:"valid1",
             nameGame:"Resident Evil",
             provider:"drive"
-        }
+        },
+        {
+            createdAt: new Date("2021-05-01"),
+            updatedAt:  new Date("2021-05-01"),
+            directorySaveGame:"./src/far-cry",
+            id:"valid2",
+            nameGame:"Far Cry",
+            provider:"drive"
+        },
+        {
+            createdAt: new Date("2021-05-01"),
+            updatedAt:  new Date("2021-05-01"),
+            directorySaveGame:"./src/Halo",
+            id:"valid3",
+            nameGame:"Halo",
+            provider:"drive"
+        },
+        {
+            createdAt: new Date("2021-05-01"),
+            updatedAt:  new Date("2021-05-01"),
+            directorySaveGame:"./src/Forza",
+            id:"valid4",
+            nameGame:"Forza",
+            provider:"drive"
+        },
+
     ] as GameSaveInterface[]
     
+    async getAll () : Promise<Either<ErrorBase, GameSaveInterface[]>>{
+        return Right.create(this.gameSaves)
+    }
+
     async removeByGameName (gameName: string) :Promise<Either<ErrorBase, void>>{
         const gameSave = this.gameSaves.find(item=>item.nameGame === gameName)
         if(!gameSave) return Left.create(new GameSaveNotFoundError())
