f1087ce8e90870b9e962f6b6232e98cd41eebedb
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-29 01:41:24 -0300

    📦  FEAT: DeleteGameSaveUseCase implemented

---
+++ src/data/useCases/deleteGameSave.ts
@@ -0,0 +1,29 @@
+import { Either, Left, Right } from "../errors/either"
+import { ErrorBase } from "../errors/errorBase"
+import { providerImp } from "../interfaces/providerInterface"
+import { gameSaveRepositoryImp } from "../interfaces/repository/gameSave"
+
+export interface DeleteGameSaveReq{
+    nameGame:string,
+    provider:"drive"|"onedrive"
+}
+
+export class DeleteGameSaveUseCase{
+
+    constructor(
+        private readonly gameSaveRepository:gameSaveRepositoryImp,
+        private readonly providerController:providerImp
+    ){
+    }
+    async exec({nameGame,provider}:DeleteGameSaveReq):Promise<Either<ErrorBase,void>>{
+        
+        const gameSaveDeleteFromProvider = await this.providerController.deleteGameSave(provider,nameGame)
+        if(gameSaveDeleteFromProvider.left) return Left.create(gameSaveDeleteFromProvider.left)
+        
+        const gameSaveToDelete = await this.gameSaveRepository.deleteByGameName(nameGame)
+        if(gameSaveToDelete.left) return Left.create(gameSaveToDelete.left)
+
+        return Right.create(undefined)
+    }
+
+}
