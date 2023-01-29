14235d436983f6fde780a1e372b90c4695c4317a
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-29 01:42:03 -0300

    📦  FEAT: new method add deleteByGameName

--- src/data/interfaces/repository/gameSave.ts
+++ src/data/interfaces/repository/gameSave.ts
@@ -11,5 +11,6 @@ import { GameSaveInterface } from "../gameSave";
 export interface gameSaveRepositoryImp{
     create:(gameSave:Replacer<GameSaveInterface,{id?:string,createdAt?:Date,updatedAt?:Date}>)=>Promise<Either<ErrorBase,GameSaveInterface>>
     removeByGameName:(gameName:string)=>Promise<Either<ErrorBase,void>>
+    deleteByGameName:(gameName:string)=>Promise<Either<ErrorBase,void>>
     getAll:() => Promise<Either<ErrorBase,GameSaveInterface[]>>
 }
