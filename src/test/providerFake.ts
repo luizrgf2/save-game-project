dace89ed55d233a762c7c158ab84ca1e73d6dae1
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-29 01:40:18 -0300

    📦  FEAT: deleteGameSave new method implemented

--- src/test/providerFake.ts
+++ src/test/providerFake.ts
@@ -3,6 +3,10 @@ import { ErrorBase } from "../data/errors/errorBase";
 import { providerImp } from "../data/interfaces/providerInterface";
 
 export class ProviderFake implements providerImp{
+   async deleteGameSave (provider: "drive" | "onedrive", gameName: string) : Promise<Either<ErrorBase, void>>{
+      return Right.create(undefined)
+   }
+
    async uploadSaveToProvider (provider: "drive" | "onedrive", directoryOfGameSave: string, gameName: string) : Promise<Either<ErrorBase, void>>{
         return Right.create(undefined)
    }
