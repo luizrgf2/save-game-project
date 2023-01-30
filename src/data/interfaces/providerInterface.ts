8423db4d8aa06436a55429b83365e4efefa86936
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-30 18:05:45 -0300

    ⚡  REFACTOR: change gameName to idProvider

--- src/data/interfaces/providerInterface.ts
+++ src/data/interfaces/providerInterface.ts
@@ -3,5 +3,5 @@ import { ErrorBase } from "../errors/errorBase";
 
 export interface providerImp{
     uploadSaveToProvider:(provider:"onedrive"|"drive",directoryOfGameSave:string,gameName:string)=>Promise<Either<ErrorBase,void>>
-    deleteGameSave:(provider:"drive"|"onedrive",gameName:string) =>Promise<Either<ErrorBase,void>>
+    deleteGameSave:(provider:"drive"|"onedrive",idProvider:string) =>Promise<Either<ErrorBase,void>>
 }
