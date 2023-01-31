316ac5301c54c3f293f92311c6e03f607ba2b004
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-30 22:51:29 -0300

    📦  FEAT: new parameter client:OAuth2Client add

--- src/infra/services/google/google.ts
+++ src/infra/services/google/google.ts
@@ -1,13 +1,24 @@
+import { OAuth2Client } from "google-auth-library";
 import { prismaClient } from "../../database/prismaClient";
 import { PrismaRepository } from "../../repository/prismaRepository";
 import { AuthGoogle } from "./authentication";
 import { ProviderGoogle } from "./provider";
 
 export class Google{
+    
 
-    static async exec(){
+    constructor(){
+
+    }
+
+    async getClient(){
         const auth = new AuthGoogle()
         const client = await auth.exec()
+        return client
+    }
+
+    exec(client:OAuth2Client){
+
         const repo = new PrismaRepository(prismaClient)
         const provider = new ProviderGoogle(client,repo)
         return provider
