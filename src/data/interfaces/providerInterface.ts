2aeb3f8dc0d66f064f0797e1865f01068686bc91
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-31 15:49:56 -0300

    📦  FEAT: downloadGameSave new method add

--- src/data/interfaces/providerInterface.ts
+++ src/data/interfaces/providerInterface.ts
@@ -4,4 +4,5 @@ import { ErrorBase } from "../errors/errorBase";
 export interface providerImp{
     uploadSaveToProvider:(provider:"onedrive"|"drive",directoryOfGameSave:string,gameName:string)=>Promise<Either<ErrorBase,void>>
     deleteGameSave:(provider:"drive"|"onedrive",idProvider:string) =>Promise<Either<ErrorBase,void>>
+    downloadGameSave: (idProvider:string,outputFolder:string)=>Promise<Either<ErrorBase,void>>
 }
