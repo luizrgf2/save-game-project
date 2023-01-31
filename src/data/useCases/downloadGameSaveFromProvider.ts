c0e969a1f0fb38b50f37e020d8f4f6e082bb37fd
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-31 15:51:24 -0300

    📦  FEAT: DownloadGameSaveFromProviderUseCase implemented

---
+++ src/data/useCases/downloadGameSaveFromProvider.ts
@@ -0,0 +1,26 @@
+import { Either, Left, Right } from "../errors/either"
+import { ErrorBase } from "../errors/errorBase"
+import { providerImp } from "../interfaces/providerInterface"
+import { gameSaveRepositoryImp } from "../interfaces/repository/gameSave"
+
+export interface DownloadGameSaveFromProviderReq{
+    gameName:string,
+    outputFolder:string
+}
+
+export interface DownloadGameSaveFromProviderRes{
+    message:string
+}
+
+export class DownloadGameSaveFromProviderUseCase{
+    
+    constructor(private readonly gameSaveRepository:gameSaveRepositoryImp,private readonly provider:providerImp){}
+    
+    async exec({gameName,outputFolder}:DownloadGameSaveFromProviderReq):Promise<Either<ErrorBase,DownloadGameSaveFromProviderRes>>{
+        const gameSaveToDownload = await this.gameSaveRepository.getGameSaveWithGameName(gameName)
+        if(gameSaveToDownload.left) return Left.create(gameSaveToDownload.left)
+        const downloadGameSave = await this.provider.downloadGameSave(gameSaveToDownload.right.idProvider||"",outputFolder)
+        if(downloadGameSave.left) return Left.create(downloadGameSave.left)
+        return Right.create({message:"Download completo e salvo!"})
+    }
+}
