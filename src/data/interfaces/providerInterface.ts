d98c3d353724127de52a9e6f6fa028534c291114
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-31 16:13:32 -0300

    📦  FEAT: new field provider add

--- src/data/interfaces/providerInterface.ts
+++ src/data/interfaces/providerInterface.ts
@@ -4,5 +4,5 @@ import { ErrorBase } from "../errors/errorBase";
 export interface providerImp{
     uploadSaveToProvider:(provider:"onedrive"|"drive",directoryOfGameSave:string,gameName:string)=>Promise<Either<ErrorBase,void>>
     deleteGameSave:(provider:"drive"|"onedrive",idProvider:string) =>Promise<Either<ErrorBase,void>>
-    downloadGameSave: (idProvider:string,outputFolder:string)=>Promise<Either<ErrorBase,void>>
+    downloadGameSave: (idProvider:string,outputFolder:string,provider:"drive"|"onedrive")=>Promise<Either<ErrorBase,void>>
 }
