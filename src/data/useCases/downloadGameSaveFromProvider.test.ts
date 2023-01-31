fb73c8017f508408313aadcbbcd637b51a680285
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-31 15:51:04 -0300

    ✅  TEST: downloadGameSaveFromProvider test implemented

---
+++ src/data/useCases/downloadGameSaveFromProvider.test.ts
@@ -0,0 +1,20 @@
+import { ProviderFake } from "../../test/providerFake"
+import { GameSaveRepositoryFake } from "../../test/repository/gameSave"
+import { DownloadGameSaveFromProviderUseCase } from "./downloadGameSaveFromProvider"
+
+describe("downloadGameSaveFromProvider", function(){
+
+    const gameSaveRepository = new GameSaveRepositoryFake()
+    const provider = new ProviderFake()
+
+    it("should be able download saves from provider selected", async function(){
+        const sut = new DownloadGameSaveFromProviderUseCase(gameSaveRepository,provider)
+        const res = await sut.exec({
+            gameName:"Resident Evil",
+            outputFolder:"./saves"
+        })
+        expect(res.left).toBeFalsy()
+        expect(res.right?.message).not.toBe(undefined)
+    })
+
+})
