c22208f379bd52853cc679ff109d90037a3e793f
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-31 16:14:03 -0300

    📦  FEAT: provider new field add

--- src/data/useCases/downloadGameSaveFromProvider.ts
+++ src/data/useCases/downloadGameSaveFromProvider.ts
@@ -5,7 +5,8 @@ import { gameSaveRepositoryImp } from "../interfaces/repository/gameSave"
 
 export interface DownloadGameSaveFromProviderReq{
     gameName:string,
-    outputFolder:string
+    outputFolder:string,
+    provider:"drive"|"onedrive"
 }
 
 export interface DownloadGameSaveFromProviderRes{
@@ -16,10 +17,10 @@ export class DownloadGameSaveFromProviderUseCase{
     
     constructor(private readonly gameSaveRepository:gameSaveRepositoryImp,private readonly provider:providerImp){}
     
-    async exec({gameName,outputFolder}:DownloadGameSaveFromProviderReq):Promise<Either<ErrorBase,DownloadGameSaveFromProviderRes>>{
+    async exec({gameName,outputFolder,provider}:DownloadGameSaveFromProviderReq):Promise<Either<ErrorBase,DownloadGameSaveFromProviderRes>>{
         const gameSaveToDownload = await this.gameSaveRepository.getGameSaveWithGameName(gameName)
         if(gameSaveToDownload.left) return Left.create(gameSaveToDownload.left)
-        const downloadGameSave = await this.provider.downloadGameSave(gameSaveToDownload.right.idProvider||"",outputFolder)
+        const downloadGameSave = await this.provider.downloadGameSave(gameSaveToDownload.right.idProvider||"",outputFolder,provider)
         if(downloadGameSave.left) return Left.create(downloadGameSave.left)
         return Right.create({message:"Download completo e salvo!"})
     }
