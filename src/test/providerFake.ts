d97e6f976721112c9f4f34828bb0a54249905544
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-01-31 15:53:17 -0300

    📦  FEAT: new method ProviderFake created

--- src/test/providerFake.ts
+++ src/test/providerFake.ts
@@ -3,6 +3,10 @@ import { ErrorBase } from "../data/errors/errorBase";
 import { providerImp } from "../data/interfaces/providerInterface";
 
 export class ProviderFake implements providerImp{
+   async downloadGameSave (idProvider: string, outputFolder: string) : Promise<Either<ErrorBase, void>>{
+      return Right.create(undefined)
+   }
+
    async deleteGameSave (provider: "drive" | "onedrive", gameName: string) : Promise<Either<ErrorBase, void>>{
       return Right.create(undefined)
    }
