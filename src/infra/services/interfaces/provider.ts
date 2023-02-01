fbaf935a39a80d32b3519453f26493c2ddd80a44
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-02-01 11:57:05 -0300

    📦  FEAT: add new method download

--- src/infra/services/interfaces/provider.ts
+++ src/infra/services/interfaces/provider.ts
@@ -1,4 +1,7 @@
+import fs from 'fs'
+
 export interface ProviderInterface{
     upload:(filePath:string,gameName:string)=>Promise<Error|undefined>
     delete:(id:string)=>Promise<Error|undefined>
+    download:(id:string)=>Promise<Error|fs.WriteStream>
 }
